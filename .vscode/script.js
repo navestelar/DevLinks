5c87ff12e340233ade33662bd31a52b8433da4b2
navestelar <acch.escola@gmail.com>
2023-02-03 21:23:18 -0300

    add readme

--- .vscode/script.js
+++ .vscode/script.js
@@ -3,8 +3,8 @@ function toggleMode() {
   html.classList.toggle("light")
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./Avatar-light.png")
+    img.setAttribute("src", "./assets/Avatar-light.png")
   } else {
-    img.setAttribute("src", "./Avatar.png")
+    img.setAttribute("src", "./assets/Avatar.png")
   }
 }
